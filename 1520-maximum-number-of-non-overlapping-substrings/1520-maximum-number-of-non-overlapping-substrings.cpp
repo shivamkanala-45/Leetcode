71c8766346689b71e251324047218fd4f2680d98
Kanala shivam <shivamkanala@gmail.com>
2026-09-18 19:17:44 +0530

    Time: 7 ms (84.19%), Space: 23.4 MB (71.64%) - LeetHub

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        int n = s.size();
+
+        vector<int> first(26, n);
+        vector<int> last(26, -1);
+
+        for (int i = 0; i < n; i++) {
+            int c = s[i] - 'a';
+            first[c] = min(first[c], i);
+            last[c] = i;
+        }
+
+        vector<pair<int, int>> intervals;
+
+        for (int c = 0; c < 26; c++) {
+            if (last[c] == -1) continue;
+
+            int l = first[c];
+            int r = last[c];
+            bool valid = true;
+
+            for (int i = l; i <= r; i++) {
+                int x = s[i] - 'a';
+
+                if (first[x] < l) {
+                    valid = false;
+                    break;
+                }
+
+                r = max(r, last[x]);
+            }
+
+            if (valid)
+                intervals.push_back({l, r});
+        }
+
+        sort(intervals.begin(), intervals.end(),
+             [](auto &a, auto &b) {
+                 return a.second < b.second;
+             });
+
+        vector<string> ans;
+        int end = -1;
+
+        for (auto [l, r] : intervals) {
+            if (l > end) {
+                ans.push_back(s.substr(l, r - l + 1));
+                end = r;
+            }
+        }
+
+        return ans;
+    }
+};
